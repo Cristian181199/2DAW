3a97bae519b1eea590ba0f6749c57109d8785425
Cristian Quintero García <cristian181199@gmail.com>
2020-10-08 21:21:32 +0200

    DWES SIGUIENDO UN POCO A RICPELO

---
+++ DWES/funciones.php
@@ -0,0 +1,19 @@
+<?php
+
+function suma($n1, $n2)
+{
+    return $n1 + $n2;
+}
+
+echo suma(3, 5);
+
+function doble($n)
+{
+    return 2 * $n;
+}
+
+echo doble(5);
+
+$num  = 4;
+doble($num);
+echo $num;
