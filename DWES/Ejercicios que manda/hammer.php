0fc7244c9f3aa6859db80514dd57793713abdc5a
Cristian Quintero García <cristian181199@gmail.com>
2020-10-09 20:08:00 +0200

    DWES, EJERCICIO HAMMER, AÑADIDA LA SOLUCION AL TEST 7 QUE COMPARA LA LONGITUD DE LAS 2 CADENAS Y DEVUELVE FALSE SI NO COINCIDEN.

---
+++ DWES/Ejercicios que manda/hammer.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Esto es sólo un ESQUELETO para el ejercicio de la "distancia Hamming".
+ */
+
+function distancia($a, $b)
+{
+    if (mb_strlen($a) != mb_strlen($b)) {
+        return false;
+    }
+    //
+    // TU CÓDIGO VA AQUÍ
+    //
+}
