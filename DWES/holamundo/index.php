d357dbff5b2c83828c60a72f43538b99da5d1976
Cristian Quintero García <cristian181199@gmail.com>
2020-10-16 18:07:20 +0200

    Initial commit

--- DWES/holamundo/index.php
+++ DWES/holamundo/index.php
@@ -1,17 +1,19 @@
 <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hola Mundo</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hola Mundo</title>
+    </head>
 
-<body>
-    <form action="saluda.php" method="get">
-        <label for="nom"> Escribe tu nombre: </label>
-            <input type="text" name="nombre" id="nom">
+    <body>
+        <form action="saluda.php" method="get">
+            <label for="nom"> Escribe tu nombre:* </label>
+            <input type="text" name="nombre" id="nom"> <br>
+            <label for="anyo"> Escribe tu año de nacimiento: </label>
+            <input type="text" name="anyo" id="anyo" size="10"> <br>
             <button type="submit">Saludar</button>
-    </form>
-</body>
+        </form>
+    </body>
 
 </html>
