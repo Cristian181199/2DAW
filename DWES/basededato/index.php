188081126a97cdc231bac6b4bc78950c3ee66957
Cristian Quintero García <cristian181199@gmail.com>
2020-10-27 19:09:40 +0100

    ACTUALIZADO DE RICPELO

--- DWES/basededato/index.php
+++ DWES/basededato/index.php
@@ -6,8 +6,9 @@
     <title>Departamentos</title>
 </head>
 <body>
+<body>
     <?php
-    $dept_no = isset($_GET['dept_no']) ? trim($_GET['dept_no']) : null;
+    $dept_no = isset($_GET['dept_no']) ? trim($_GET['dept_no']) : '';
     ?>
     <form action="" method="get">
         <label for="dept_no">Número:</label>
@@ -16,9 +17,22 @@
     </form>
     <?php
     $pdo = new PDO('pgsql:host=localhost;dbname=prueba', 'cristian', 'cristian');
-    $sent = $pdo->query('SELECT COUNT(*) FROM depart');
-    $count = $sent->fetchColumn();
-    $sent = $pdo->query('SELECT * FROM depart');
+    // Validación del $dept_no
+    if ($dept_no == '') {
+        $sent = $pdo->query('SELECT COUNT(*) FROM depart');
+        $count = $sent->fetchColumn();
+        $sent = $pdo->query('SELECT * FROM depart');
+    } else {
+        $sent = $pdo->prepare("SELECT COUNT(*)
+                                 FROM depart
+                                WHERE dept_no = :dept_no");
+        $sent->execute([':dept_no' => $dept_no]);
+        $count = $sent->fetchColumn();
+        $sent = $pdo->prepare("SELECT *
+                                 FROM depart
+                                WHERE dept_no = :dept_no");
+        $sent->execute([':dept_no' => $dept_no]);
+    }
     ?>
     <h3>La tabla tiene <?= $count ?> filas.</h3>
     <table border="1">
@@ -26,6 +40,7 @@
             <th>Núm. Dept.</th>
             <th>Nombre</th>
             <th>Localidad</th>
+            <th>Acciones</th>
         </thead>
         <tbody>
             <?php foreach ($sent as $fila): ?>
@@ -33,6 +48,12 @@
                     <td><?= $fila['dept_no'] ?></td>
                     <td><?= $fila['dnombre'] ?></td>
                     <td><?= $fila['loc'] ?></td>
+                    <td>
+                        <form action="borrar.php" method="post">
+                            <input type="hidden" name="id" value="<?= $fila['id'] ?>">
+                            <button type="submit">Borrar</button>
+                        </form>
+                    </td>
                 </tr>
             <?php endforeach ?>
         </tbody>
